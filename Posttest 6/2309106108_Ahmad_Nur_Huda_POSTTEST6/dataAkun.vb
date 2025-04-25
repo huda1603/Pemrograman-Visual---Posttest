8016aaba4bbbeb38060392faca7c1b1fc98af8b4
ahmadnurhuda <frotnalgamingch300100@gmail.com>
2025-04-25 20:58:09 +0700

    Delete Posttest 6/2309106108_Ahmad_Nur_Huda_POSTTEST6 directory

--- Posttest 6/2309106108_Ahmad_Nur_Huda_POSTTEST6/dataAkun.vb
+++
@@ -1,5 +0,0 @@
-﻿Module dataAkun
-    Public users As New Dictionary(Of String, String)
-    Public userFilmData As New Dictionary(Of String, List(Of String))
-    Public currentUser As String = Nothing
-End Module
