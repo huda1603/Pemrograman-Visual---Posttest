7ec8ffba721df3bd9ab7f751034b2bdc61f72ddd
ahmadnurhuda <frotnalgamingch300100@gmail.com>
2025-05-09 21:15:25 +0800

    Update Form2.vb

--- 2309106108_Ahmad_Nur_Huda_Tugas_Loading_Dan_Animasi/Form2.vb
+++ 2309106108_Ahmad_Nur_Huda_Tugas_Loading_Dan_Animasi/Form2.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form2
     Private Sub btnKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        Dim result As DialogResult = MessageBox.Show("Apakah Anda yakin ingin keluar?", "Konfirmasi Keluar", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
         Dim f1 As New Form1()
         f1.Show()
         Me.Hide()
